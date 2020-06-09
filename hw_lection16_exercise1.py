5372ebf63e00269152fbd78696b3f37fe464e553
EY-cmyk <61290430+EY-cmyk@users.noreply.github.com>
2020-06-09 11:29:51 +0300

    Add files via upload

---
+++ hw_lection16_exercise1.py
@@ -0,0 +1,7 @@
+programmming = ["Alexsik#8119", "Brighton#7027", "Julia Tkach#0760", "komenchuk.oleh#7966", "Nikita000#7889", "DinozavR#9817", "Dima0000#3097"]
+print(type(programmming))
+
+enum = enumerate(programmming)
+for i in enumerate(programmming):
+    print(i)
+print(list(enum))
